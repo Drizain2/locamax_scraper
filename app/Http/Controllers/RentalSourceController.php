5f794ab630853965295de071bae634c5add56491
Drizain <drissa.traore@epitech.eu>
2025-12-15 09:51:35 +0000

    .[ADD] Ajout du contreoller:posibilité de la fonctionnalité de recherche par ville et renvois sur la vue annonceListe, ajout du model avec ses fillables

---
+++ app/Http/Controllers/RentalSourceController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RentalSource;
+use Illuminate\Http\Request;
+
+class RentalSourceController extends Controller
+{
+    public function index(Request $request)
+    {
+        $city = $request->input("city");
+        $query = RentalSource::query();
+
+        if ($city) {
+            $query->where('city', $city);
+        }
+
+        $cities = RentalSource::select("city")->distinct()->pluck('city');
+
+        return view("annonceListe", [
+            "rentals" => $query->get(),
+            'cities' => $cities,
+            'selectedCity' => $city
+        ]);
+
+    }
+}
