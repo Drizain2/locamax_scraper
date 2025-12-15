2f5a6cbc31dabbd73bb54d8742aa506efc78db85
Drizain <drissa.traore@epitech.eu>
2025-12-15 09:34:07 +0000

    [INITIATION] init du projet avec modelisation et migration de la Base de donnée

---
+++ database/migrations/2025_12_15_091251_create_rentale_sources_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rentale_sources', function (Blueprint $table) {
+            $table->id();
+            $table->string('source_url')->unique();
+            $table->string("source_type");
+            $table->string("name_or_title");
+            $table->string("phone_number")->nullable();
+            $table->string("email")->nullable();
+            $table->string("property_type")->nullable();
+            $table->string("city")->nullable();
+            $table->string("district")->nullable();
+            $table->boolean("is_qualified")->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rentale_sources');
+    }
+};
